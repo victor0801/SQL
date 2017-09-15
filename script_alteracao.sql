56a2bac612460e759c6219d2475da4b800582c14
Victor Vinicius Gonçalves Barbosa <victorvinicius.0801@hotmail.com>
2017-09-15 20:21:23 -0300

    Update script_alteracao.sql

--- script_alteracao.sql
+++ script_alteracao.sql
@@ -7,8 +7,8 @@ Eduardo Marinho RA 1700158
 SI 2B - NOITE
  */
 
-alter table cliente add nome varchar(300) Not null
-alter table cliente drop constraint uqclienterg (rg,uf),
+alter table cliente alter column nome varchar(300) Not null
+alter table cliente drop constraint uqclienterg
 alter table cliente add tel char (14)
 
 alter table veiculo drop column km
